f91a7f0f61f76ec5e4beb3e1bac94908d17badda
Brindusa22 <>
2023-11-01 14:34:29 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class GameBoard:
+    """
+    Main game board class. Sets the board size, plants random bombs,
+    prints the game instructions. Has methods to uncover the cells,
+    to calculate the neighbour cells and to check if the game is won or lost
+    """
+
+    def __init__(self, size, bombs):
+        self.size = size
+        self.bombs = bombs
+        self.board = [["." for x in range(size)] for y in range(size)]
+        
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
+            
+
+game = GameBoard(8, 4)
+game.print_board()
