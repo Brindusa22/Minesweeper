bf43472403071f224fd11ba8a7ff8ee703b226cf
Brindusa22 <>
2023-11-03 20:17:59 +0000

    Add function to count neighboring bombs of a cell

--- run.py
+++ run.py
@@ -20,14 +20,14 @@ class GameBoard:
         # print the column number
         col_no = "  "
         for i in range(self.size):
-            col_no = col_no + "   " + str(i+1)
+            col_no = col_no + "   " + str(i)
         print(col_no)
 
         print("    " + "----" * self.size)
 
         # add the rows with row numbers and the delimitation between them
         for i in range(self.size):
-            print(f"{i+1:2} | " + " ".join(self.board[i]))
+            print(f"{i:2} | " + " ".join(self.board[i]))
             print("    " + "----" * self.size)
 
     def plant_bombs(self):
@@ -44,9 +44,27 @@ class GameBoard:
                 continue
             else:
                 self.board[row][col] = "* |"
-                planted_bombs += 1 
+                planted_bombs += 1
+
+    def neighboring_bombs(self, row, col):
+        """
+         Identify number of neighboring bombs """
+
+        bomb_no = 0
+        for x in range(row - 1, row + 2):
+            for y in range(col - 1, col + 2):
+                if x == row and y == col:
+                    continue
+                if 0 <= x < self.size and 0 <= y < self.size:
+                    if self.board[x][y] == "* |":
+                        bomb_no += 1
+
+        return bomb_no
 
 
 game = GameBoard(6, 9)
-# game.plant_bombs()
+game.plant_bombs()
+row, col = 4, 4
+result = game.neighboring_bombs(row, col)
+print(result)
 game.print_board()
